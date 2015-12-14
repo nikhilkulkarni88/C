b7bca0402889d1a10fea20dc863ca0747f3cc984
ee597 <ee597@ee597-VB.(none)>
2015-12-13 23:28:35 -0800

    Adding 1-11 to repo

---
+++ chapter1/1-11.c
@@ -0,0 +1,38 @@
+/* Character,word and space counting program */
+#include "header.h"
+
+#define IN  1
+#define OUT 0
+
+int main()
+{
+    char a[] = "There is one	tab space '\n',1 newline char";
+
+    char *ptr = a;
+    int state = OUT;
+    int numWord = 0, numLine = 0, numChar = 0;
+    
+    while(*ptr != '\0')
+    {
+        numChar++;
+
+        if(*ptr == '\n')
+            numLine++;
+        if((*ptr == ' ') || (*ptr == '\n') || (*ptr == '\t'))
+            state = OUT;
+        else if(state == OUT)
+        {    
+            state = IN;
+            numWord++;
+        }
+        ptr++;
+    }
+
+    printf("# of Chars=%d,# of words = %d, #of newlines = %d\n",numChar,numWord,numLine);
+
+    return 0;
+
+}
+
+
+
